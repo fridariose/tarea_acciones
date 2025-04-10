b2955a1cd2530114718da3de08a68a21be0c7b7c
fridariose <161908582+fridariose@users.noreply.github.com>
2025-04-09 20:45:28 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Frida Rios", layout="wide")
+
+st.title("Frida Rios")
+st.write("Tarea Finanzas")
+#st.write("Selecciona una opción en el menú de la izquierda para continuar.")
+
+# Streamlit muestra automáticamente las páginas dentro de "pages/"
+st.sidebar.success("Selecciona una opción en el menú.")
